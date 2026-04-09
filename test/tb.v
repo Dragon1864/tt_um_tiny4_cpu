6fd0bc659e39ccbbb65c20ae720afd8bb43b0d87
Garv R Jain <122778557+Dragon1864@users.noreply.github.com>
2026-04-09 20:33:07 +0530

    Update tb.v
    For FPGA

--- test/tb.v
+++ test/tb.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module tb;
 
     reg clk;
@@ -5,12 +7,12 @@ module tb;
     reg ena;
 
     reg [7:0] ui_in;
+    reg [7:0] uio_in;
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
-    reg  [7:0] uio_in;
 
-    tt_um_tiny4_cpu dut (
+    tt_um_4plc dut (
         .clk(clk),
         .rst_n(rst_n),
         .ui_in(ui_in),
@@ -20,5 +22,26 @@ module tb;
         .uio_in(uio_in),
         .ena(ena)
     );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        rst_n = 0;
+        ena   = 1;
+        ui_in = 0;
+        uio_in = 0;
+
+        #20;
+        rst_n = 1;
+
+        // simple stimulus
+        uio_in = 8'b10000110; // AND + XOR config
+        ui_in  = 8'b00001111;
+
+        #100;
+
+        $finish;
+    end
 
 endmodule
